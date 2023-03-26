103c33c7d5644fb4745edd2010db3aa7e2b37d86
Xerkkun <xerk.kun@gmail.com>
2023-03-26 15:05:14 -0600

    changes on sum_x vector

--- gl_test.py
+++ gl_test.py
@@ -12,15 +12,9 @@ import matplotlib.pyplot as plt
 #Funciones
 #===============================================================================
 #Ecuaciones del sistema de Lorenz
-def lorenz_frac(x):
-    # Definición de parámetros del sistema
-    sigma = 10.0
-    beta = 8./3.
-    rho = 28.0
+def f(x):
     xdot = np.zeros_like(x)
-    xdot[0] = sigma * (x[1] - x[0])
-    xdot[1] = rho * x[0] - x[1] - x[0] * x[2]
-    xdot[2] = -beta * x[2] + x[0] * x[1]
+    xdot = x**2
     return xdot
 
 #coeficientes binomiales
@@ -30,7 +24,6 @@ def binomial_coef(alpha,k,nu):
     for j in range(1,k+nu):
         c[j] = c[j-1] - (c[j-1]*(1+alpha)/j)
         #c[j] = int(c*10**10)/10**10
-        print(c[j])
     return c
 
 # Definición del método de Grünwald-Letnikov para la derivada de orden fraccionario
@@ -38,39 +31,34 @@ def grunwald_letnikov(x,h_alpha,k,alpha,xt,nu):
     # Integración numérica del sistema de Lorenz con Grünwald-Letnikov
 
     # Iteraciones del método
-    sum_x = np.zeros(3)
-    prod_x = np.zeros((k,3))
+    sum_x = 0
+
     c = binomial_coef(alpha,k,nu)
-    #print(c)
-    #for i in range(1,k+1):
-    for i in range(1,10):
+    for i in range(1,k+1):
         # Se calculan las sumas de los coeficientes binomiales en cada iteracion
-        prod_x[k-i,:] = c[k-i+1]*x[i-1,:]
-        sum_x = sum_x + prod_x[k-i,:]
-        #print(c[k-i+1],x[i-1,:])
-        #print(sum_x[0],sum_x[1],sum_x[2])
-        # Las variables x,y,z se evaluan con el vector de tiempo
-        x[i,:] = lorenz_frac(x[i-1,:])*h_alpha - sum_x
-        #if i%50 == 0:
-            #print(i,x[i,0],x[i,1],x[i,2])
-            #print(prod_x[i])
+        for j in range(nu,k+1):
+            sum_x += c[j]*x[k-j,0]
+            # Las variables x,y,z se evaluan con el vector de tiempo
+        x[i,0] = f(x[i-1,0])*h_alpha - sum_x
+        print(i,x[i-1,0])
+        sum_x = 0
 
     return x
 
 # Definición del orden fraccionario
-alpha = 0.9935
+alpha = 0.9
 
 # Definición del vector de estado inicial
-x0 = np.array([1.0, 1.0, 1.0])
+x0 = 1.0
 
 # Definición del intervalo de tiempo y el paso de integración
 t0 = 0.0
-tf = 100.0
-h = 0.001
+tf = 1.0
+h = 0.01
 h_alpha = h**alpha
 
 # Longitud de memoria
-Lm = 500
+Lm = 10
 
 # Número de coeficientes binomiales
 m = Lm/h
@@ -83,28 +71,23 @@ if k<=m:
     nu = 1
 else:
     nu = k - m
-#print(k,m,nu)
 
 # Inicialización del vector de estado y tiempo
-x = np.zeros((k+1, 3))
+x = np.zeros((k+1, 1))
 t = np.linspace(t0, tf, len(x))
 xt = np.zeros_like(x)
 xt[:,0] = t
-xt[:,1] = t
-xt[:,2] = t
-#print(lorenz_frac(xt[100,:]))
 
 # Condición inicial
-x[0,:] = x0
-#print(x)
+x[0,0] = x0
+
 x = grunwald_letnikov(x,h_alpha,k,alpha,xt,nu)
-#print(lorenz_frac(xt[0,:]))
 
 # Gráfica de la solución
-fig = plt.figure(figsize=(10, 8))
-ax = fig.add_subplot(111, projection='3d')
-ax.plot(x[:,0], x[:,1], x[:,2])
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
-plt.show()
+# fig = plt.figure(figsize=(10, 8))
+# ax = fig.add_subplot(111, projection='3d')
+# ax.plot(x[:,0], x[:,1], x[:,2])
+# ax.set_xlabel('x')
+# ax.set_ylabel('y')
+# ax.set_zlabel('z')
+# plt.show()
